fff4c1d6ebadaca326c79b7583cbe488988206ca
ronalddenouden <ronalddenouden123@gmail.com>
2022-08-14 16:09:13 +0200

    Removed addbomb function and added a few comments as well as changing the gameActive boolean to an enumerator called GameState.

--- Minesweeper/MinesweeperGame.cpp
+++ Minesweeper/MinesweeperGame.cpp
@@ -16,20 +16,19 @@ MinesweeperGame::MinesweeperGame(int difficulty, bool solvable, int width, int h
 // Competition
 void MinesweeperGame::populateBombsGrid(int difficulty, bool solvable)
 {
+	// add to argument first click location (which can't be a bomb when generating)
+	
 	// Calculate nearby numbers in populateNumbersGrid
 
-
+	// make an array with coordinates of bombs: [[x1,y1],[x2,y2],...[xn,yn]]
 
 	bombsAmount = 0;
 }
 
 void MinesweeperGame::populateNumbersGrid() {
-
-}
-
-void MinesweeperGame::addBombToGrid(int x, int y) {
-	grid[y][x] = -1;
-	// increase numbers around it in the grid by one.
+	// make empty grid or fill the grid with 0's
+	// go through array with coordinates of bombs and call addBombToGrid
+	// put -1 on bomb locations and increase numbers around it in the grid by one.
 }
 
 // What does a tile in this backend grid represent?
@@ -42,8 +41,8 @@ bool MinesweeperGame::updateGrid(int x, int y)
 		// if left clicked if there is a bomb or not
 			// if there is a bomb gameover return false (keep track somewhere that its gameover)
 			// if there is no bomb return true 
-	if (grid[y][x] <= -1) {
-		gameActive = false;
+	if (grid[y][x] == -1) {
+		gameState = GameState::lost;
 		return false;
 	}
 	else {
@@ -54,5 +53,6 @@ bool MinesweeperGame::updateGrid(int x, int y)
 // Return number so that the front end can display it if its open.
 int MinesweeperGame::getTileNumber(int x, int y) {
 	return grid[y][x];
+	// error if its a bomb?
 }
 
--- Minesweeper/MinesweeperGame.h
+++ Minesweeper/MinesweeperGame.h
@@ -14,19 +14,23 @@ private:
 	// Method to populate the grid with the numbers according to the bombs
 	void populateNumbersGrid();
 	// Method to update the grid, when a particular tile has been pressed
-	bool updateGrid(int x, int y); 
+	bool updateGrid(int x, int y);
 	// Method to get the number of a tile to display, where -1 is a bomb and 0-8 are numbers to display.
 	int getTileNumber(int x, int y);
-	// Method to add a bomb to the grid which increases the numbers around it by one and sets its position to -1.
-	void addBombToGrid(int x, int y);
 
 	// Points to the array where will store the grid
-	int** grid; 
-
+	int** grid;
 	// Keeps track if the game is still running when true or if it is a game over when false
-	int gameActive;
-
+	enum GameState {
+		// states can still be changed
+		initializing = 0,
+		active = 1,
+		won = 2,
+		lost = 3
+	};
+	// keeps track of the gameState.
+	GameState gameState;
 	// Keeps track of amount of bombs for this game instance
 	int bombsAmount;
-};
 
+};
