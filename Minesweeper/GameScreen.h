56f88c5617f29701050820ca0f4cba66ebf9cdcb
ronalddenouden <ronalddenouden123@gmail.com>
2022-08-20 14:10:44 +0200

    Merge main into Backbone

--- Minesweeper/GameScreen.h
+++ Minesweeper/GameScreen.h
@@ -2,41 +2,58 @@
 #include <wx/panel.h>
 #include <wx/button.h>
 #include <wx/bitmap.h>
+#include <wx/event.h>
 
 #include "MinesweeperGame.h"
 
 class GameScreen : public wxPanel {
 public:
+	// GameScreen constructor
 	GameScreen(wxWindow* parent);
-	
-	void UpdateTile(wxCommandEvent& event);
 
-	class TileButton : public wxButton {	
+
+	// Event listener for size changes
+	void resize(wxSizeEvent& event);
+
+	// A visual tile in the grid
+	// For some reference, see https://wiki.wxwidgets.org/Painting_your_custom_control
+	class wxTile : public wxWindow {
 	public:
-		// Constructor
-		TileButton(wxWindow* parent, wxPoint pos, wxSize sz, int x, int y);
+		// wxTile constructor
+		wxTile(wxWindow* parent, int x, int y);
 
-		// Overrides the normal drawing function, draws button according to state
-		void Update() override;
-
-		//Coordinates in the grid
-		int x, y;
-
-		//Keeps track of the current state of the tile
-		int CurrentState;
-
-		//Every possible state for the tile
-		enum TileState {
-			flag = 0,
-			bomb = 1,
-			closed = 2,
-			open = 3
+		// The state of a tile
+		enum State {
+			flag,
+			bomb,
+			closed,
+			open
 		};
 
+		// The UI size of tiles
+		static const int size = 30;
+
+		// Paint event handler
+		void paintEvent(wxPaintEvent& evt);
+
+		// Left click event handler
+		void leftClick(wxMouseEvent& event);
+		// Reft click event handler
+		void rightClick(wxMouseEvent& event);
+
+	private:
+		// The coordinates of this tile
+		int x, y;
+
+		// The state of this tile
+		State state;
+
+		// Required for the event table in the implementation file (see https://docs.wxwidgets.org/3.2/overview_events.html)
+		wxDECLARE_EVENT_TABLE();
 	};
-	
+
 	// Dynamic array to all the tiles
-	TileButton*** tiles;
+	wxTile*** tiles;
 
 private:
 	// Keep track of the amount of current flags on the grid
